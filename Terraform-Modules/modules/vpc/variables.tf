db84edcd11d0d77152e6712fd0b80ae149ce25a7
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-08-02 19:44:10 +0530

    Create variables.tf

---
+++ Terraform-Modules/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones for the subnets"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+}
