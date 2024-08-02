3f4b635c494a592b4ec1af9c05a6a5017e0d429e
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-08-02 19:46:28 +0530

    Update main.tf

--- Terraform-Modules/modules/security-groups/main.tf
+++ Terraform-Modules/modules/security-groups/main.tf
@@ -1,1 +1,23 @@
-Done
+resource "aws_security_group" "bastion" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, { Name = "Bastion Security Group" })
+}
+
+output "bastion_sg_id" {
+  value = aws_security_group.bastion.id
+}
