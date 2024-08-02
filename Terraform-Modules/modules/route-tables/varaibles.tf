ba8c8f2a88847d810aadf1cc6c0bb21699a68a39
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-08-02 19:45:19 +0530

    Create varaibles.tf

---
+++ Terraform-Modules/modules/route-tables/varaibles.tf
@@ -0,0 +1,29 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "Public Subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnet_ids" {
+  description = "Private Subnet IDs"
+  type        = list(string)
+}
+
+variable "igw_id" {
+  description = "Internet Gateway ID"
+  type        = string
+}
+
+variable "nat_gateway_id" {
+  description = "NAT Gateway ID"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+}
