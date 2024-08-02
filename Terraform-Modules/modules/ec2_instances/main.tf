796d6964f0f2389ea6e7d15e97ef6bca986fee45
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-08-02 19:47:32 +0530

    Update main.tf

--- Terraform-Modules/modules/ec2_instances/main.tf
+++ Terraform-Modules/modules/ec2_instances/main.tf
@@ -1,1 +1,10 @@
-Done
+resource "aws_instance" "bastion" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+  subnet_id     = var.public_subnet_id
+  key_name      = var.key_name
+
+  security_group_ids = var.security_group_ids
+
+  tags = merge(var.tags, { Name = "Bastion Host" })
+}
