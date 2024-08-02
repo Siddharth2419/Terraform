5dc5e6c10265ca9e006dba18e38b55a3e6affd64
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-08-02 19:42:48 +0530

    Update output.tf

--- Terraform-Modules/output.tf
+++ Terraform-Modules/output.tf
@@ -1,1 +1,19 @@
-Done
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "igw_id" {
+  value = module.vpc.igw_id
+}
+
+output "nat_gateway_id" {
+  value = module.vpc.nat_gateway_id
+}
